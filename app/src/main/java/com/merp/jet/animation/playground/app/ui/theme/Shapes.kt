68881c91e19b734ed5fa3e0dd5492e19f9079683
PremDanej <danejprem005@gmail.com>
2025-04-24 22:08:50 +0530

    Update shapes.

--- app/src/main/java/com/merp/jet/animation/playground/app/ui/theme/Shapes.kt
+++ app/src/main/java/com/merp/jet/animation/playground/app/ui/theme/Shapes.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
-val Shapes = Shapes(
+val Shapes = Shapes().copy(
     extraSmall = RoundedCornerShape(4.dp),
     small = RoundedCornerShape(6.dp),
     medium = RoundedCornerShape(10.dp),
