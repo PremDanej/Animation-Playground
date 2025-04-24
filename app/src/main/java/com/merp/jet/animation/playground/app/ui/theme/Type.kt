d9b665623f83fa56bdbdabc3af6bfa3d354dbf99
PremDanej <danejprem005@gmail.com>
2025-04-24 22:07:53 +0530

    Update fontfamily.

--- app/src/main/java/com/merp/jet/animation/playground/app/ui/theme/Type.kt
+++ app/src/main/java/com/merp/jet/animation/playground/app/ui/theme/Type.kt
@@ -1,11 +1,8 @@
 package com.merp.jet.animation.playground.app.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography().run {
