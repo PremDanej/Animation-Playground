850baf8b34604ee1d4dcdb2f14cb8b38457b2387
PremDanej <danejprem005@gmail.com>
2025-04-30 19:37:27 +0530

    CodePreview added.

--- app/src/main/java/com/merp/jet/animation/playground/app/ui/screen/twin/usingDpStateEasing/TweenAnimationDpState.kt
+++ app/src/main/java/com/merp/jet/animation/playground/app/ui/screen/twin/usingDpStateEasing/TweenAnimationDpState.kt
@@ -1,20 +1,20 @@
 package com.merp.jet.animation.playground.app.ui.screen.twin.usingDpStateEasing
 
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Easing
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -30,7 +30,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.merp.jet.animation.playground.app.ui.screen.twin.TweenSharedViewModel
+import com.merp.jet.animation.playground.app.navigation.AnimationPlaygroundScreens.TweenCodePreviewAndGeneratorScreen
+import com.merp.jet.animation.playground.app.viewmodel.twin.TweenSharedViewModel
 import com.merp.jet.animation.playground.app.utils.Constants.COMMON_PADDING
 import com.merp.jet.animation.playground.app.utils.Constants.CONTAINER_HEIGHT
 import com.merp.jet.animation.playground.app.utils.Constants.DEFAULT_SIZE
@@ -42,16 +43,17 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @Composable
-fun TweenAnimationDpStateScreen(navController: NavHostController , viewModel: TweenSharedViewModel) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .padding(COMMON_PADDING),
+fun TweenAnimationDpStateScreen(navController: NavHostController, viewModel: TweenSharedViewModel) {
+    LazyColumn(
+        contentPadding = PaddingValues(COMMON_PADDING),
+        modifier = Modifier.fillMaxSize()
     ) {
-        // List out all easing animation
-        EasingOptions.keys.forEach { key ->
-            AnimationDpState(easingName = key)
+        items(EasingOptions.toList()) { (key, easing) ->
+            AnimationDpState(easingName = key, easing = easing) {
+                viewModel.setEasing(key, easing)
+                viewModel.animationType = "DP"
+                navController.navigate(TweenCodePreviewAndGeneratorScreen.name)
+            }
         }
     }
 }
@@ -59,11 +61,13 @@ fun TweenAnimationDpStateScreen(navController: NavHostController , viewModel: Tw
 @Composable
 fun AnimationDpState(
     easingName: String,
+    easing: Easing,
     onClick: () -> Unit = {}
 ) {
     val shape = MaterialTheme.shapes.medium
     var expanded by remember { mutableStateOf(false) }
 
+    // Auto toggle after delay
     LaunchedEffect(expanded) {
         launch {
             delay(1500)
@@ -71,49 +75,47 @@ fun AnimationDpState(
         }
     }
 
-    AnimatedVisibility(visible = true) {
-        val animatedSize by animateDpAsState(
-            targetValue = if (expanded) EXPANDED_SIZE else DEFAULT_SIZE,
-            animationSpec = tween(
-                durationMillis = DURATION_MILLIS,
-                easing = EasingOptions.getValue(easingName)
-            ), label = "BoxSize"
-        )
+    val animatedSize by animateDpAsState(
+        targetValue = if (expanded) EXPANDED_SIZE else DEFAULT_SIZE,
+        animationSpec = tween(
+            durationMillis = DURATION_MILLIS,
+            easing = easing
+        ), label = "BoxSize"
+    )
 
-        Surface(
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(CONTAINER_HEIGHT)
+            .padding(COMMON_PADDING),
+        border = BorderStroke(0.7.dp, Color.LightGray),
+        shape = shape,
+        onClick = onClick
+    ) {
+        Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(CONTAINER_HEIGHT)
-                .padding(COMMON_PADDING),
-            border = BorderStroke(0.7.dp, Color.LightGray),
-            shape = shape,
-            onClick = onClick
+                .fillMaxSize()
+                .padding(start = START_PADDING),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(start = START_PADDING),
-                verticalAlignment = Alignment.CenterVertically
+            Text(
+                text = "► ",
+            )
+            Text(
+                text = "$easingName easing",
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.weight(1f)
+            )
+            Box(
+                modifier = Modifier.size(CONTAINER_HEIGHT),
+                contentAlignment = Alignment.Center
             ) {
-                Text(
-                    text = "► ",
-                )
-                Text(
-                    text = "$easingName easing",
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis,
-                    modifier = Modifier.weight(1f)
-                )
                 Box(
-                    modifier = Modifier.size(CONTAINER_HEIGHT),
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .size(animatedSize)
-                            .background(MaterialTheme.colorScheme.onBackground, shape)
-                            .align(Alignment.Center),
-                    )
-                }
+                    modifier = Modifier
+                        .size(animatedSize)
+                        .background(MaterialTheme.colorScheme.onBackground, shape)
+                )
             }
         }
     }
