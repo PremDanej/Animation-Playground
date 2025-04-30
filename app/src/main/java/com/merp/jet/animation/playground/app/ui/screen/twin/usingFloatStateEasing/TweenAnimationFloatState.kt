5de1e24db3f01ebf89128e39de44bca1997c69c6
PremDanej <danejprem005@gmail.com>
2025-04-30 19:37:47 +0530

    CodePreview added.

--- app/src/main/java/com/merp/jet/animation/playground/app/ui/screen/twin/usingFloatStateEasing/TweenAnimationFloatState.kt
+++ app/src/main/java/com/merp/jet/animation/playground/app/ui/screen/twin/usingFloatStateEasing/TweenAnimationFloatState.kt
@@ -1,19 +1,20 @@
 package com.merp.jet.animation.playground.app.ui.screen.twin.usingFloatStateEasing
 
+import androidx.compose.animation.core.Easing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -30,6 +31,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.merp.jet.animation.playground.app.navigation.AnimationPlaygroundScreens.TweenCodePreviewAndGeneratorScreen
+import com.merp.jet.animation.playground.app.viewmodel.twin.TweenSharedViewModel
 import com.merp.jet.animation.playground.app.utils.Constants.COMMON_PADDING
 import com.merp.jet.animation.playground.app.utils.Constants.CONTAINER_HEIGHT
 import com.merp.jet.animation.playground.app.utils.Constants.DEFAULT_SCALE
@@ -42,16 +45,17 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @Composable
-fun TweenAnimationFloatStateScreen(navController: NavHostController) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .padding(COMMON_PADDING),
+fun TweenAnimationFloatStateScreen(navController: NavHostController, viewModel: TweenSharedViewModel) {
+    LazyColumn(
+        contentPadding = PaddingValues(COMMON_PADDING),
+        modifier = Modifier.fillMaxSize()
     ) {
-        // List out all easing animation
-        EasingOptions.keys.forEach { key ->
-            AnimationFloatState(easingName = key)
+        items(EasingOptions.toList()) { (key, easing) ->
+            AnimationFloatState(easingName = key, easing = easing){
+                viewModel.setEasing(key, easing)
+                viewModel.animationType = "FLOAT"
+                navController.navigate(TweenCodePreviewAndGeneratorScreen.name)
+            }
         }
     }
 }
@@ -59,6 +63,8 @@ fun TweenAnimationFloatStateScreen(navController: NavHostController) {
 @Composable
 fun AnimationFloatState(
     easingName: String,
+    easing: Easing,
+    onClick: () -> Unit = {}
 ) {
     val shape = MaterialTheme.shapes.medium
     var expanded by remember { mutableStateOf(false) }
@@ -75,7 +81,7 @@ fun AnimationFloatState(
         animationSpec = tween(
             durationMillis = DURATION_MILLIS,
             delayMillis = 0,
-            easing = EasingOptions.getValue(easingName)
+            easing = easing
         ), label = "BoxSize"
     )
 
@@ -86,6 +92,7 @@ fun AnimationFloatState(
             .padding(COMMON_PADDING),
         border = BorderStroke(0.7.dp, Color.LightGray),
         shape = shape,
+        onClick = onClick
     ) {
         Row(
             modifier = Modifier
@@ -104,13 +111,13 @@ fun AnimationFloatState(
             )
             Box(
                 modifier = Modifier.size(CONTAINER_HEIGHT),
+                contentAlignment = Alignment.Center
             ) {
                 Box(
                     modifier = Modifier
                         .size(EXPANDED_SIZE)
                         .scale(animation)
                         .background(MaterialTheme.colorScheme.onBackground, shape)
-                        .align(Alignment.Center),
                 )
             }
         }
