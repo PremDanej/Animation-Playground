6fdcbb7d0df5c5be226fadeb65c7feb09466b6cb
PremDanej <danejprem005@gmail.com>
2025-05-03 19:31:07 +0530

    Color update.

--- app/src/main/java/com/merp/jet/animation/playground/app/ui/theme/Color.kt
+++ app/src/main/java/com/merp/jet/animation/playground/app/ui/theme/Color.kt
@@ -2,14 +2,6 @@ package com.merp.jet.animation.playground.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-/*val Purple80 = Color(0xFF9E9D24)
-val PurpleGrey80 = Color(0xFF2E7D32)
-val Pink80 = Color(0xFF558B2F)
-
-val Purple40 = Color(0xFFFFC107)
-val PurpleGrey40 = Color(0xFFB1A63A)
-val Pink40 = Color(0xFFE0CE2D)*/
-
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -18,13 +10,4 @@ val Black80 = Color(0xFF1C1B1F)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
-val White40 = Color(0xFFFFFBFE)
-
-/*
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)*/
+val White40 = Color(0xFFFAF9F6)
